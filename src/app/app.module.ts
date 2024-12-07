50c072fa41420a138b06b1c9495db2f9689f6771
aptfury <blake.lemarr@outlook.com>
2024-12-07 10:15:19 -0500

    generated v1 components and interfaces with generic global SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SectionsComponent } from './sections/sections.component';
+import { BoardsComponent } from './boards/boards.component';
+import { ThreadsComponent } from './threads/threads.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreateComponent } from './create/create.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SectionsComponent,
+    BoardsComponent,
+    ThreadsComponent,
+    PostsComponent,
+    CreateComponent,
+    ProfileComponent,
+    AdminViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/boards/boards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards',
+  templateUrl: './boards.component.html',
+  styleUrl: './boards.component.scss'
+})
+export class BoardsComponent {
+
+}
